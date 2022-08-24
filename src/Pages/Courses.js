25af01cebd6a7ef757fccbd00607cf3abecc8181
dkersten <dkersten1994@gmail.com>
2022-08-24 16:00:12 -0400

    added stats overview section to courses page

--- src/Pages/Courses.js
+++ src/Pages/Courses.js
@@ -1,13 +1,40 @@
 // components
 import Layout from "../Layout"
 import Table from "../Components/Table"
+import CardOverview from "../Components/CardOverview"
 
 // data
 import courses from "../Temp Data/CoursesData"
 
+// assets
+import { ReactComponent as GolfClubIcon} from '../Assets/Icons/golf-club-light-icon.svg'
+import { ReactComponent as AirlineTicketIcon } from '../Assets/Icons/airline-ticket.svg'
+import { ReactComponent as GlobeIcon } from '../Assets/Icons/globe-icon.svg'
+
 const Courses = () => {
     return(
         <Layout>
+            <div className="overview-section">
+                <div className="card-container">
+                    <ul>
+                        <CardOverview 
+                            icon={<GolfClubIcon />}
+                            headline="Courses Played"
+                            data={courses.totalPlayed}
+                        />
+                        <CardOverview 
+                            icon={<AirlineTicketIcon style={{ transform: 'rotate(-8deg)' }} />} 
+                            headline="States Played"
+                            data={courses.statesPlayed}
+                        />
+                        <CardOverview 
+                            icon={<GlobeIcon />} 
+                            headline="Countries Played"
+                            data={courses.countriesPlayed}
+                        />
+                    </ul>
+                </div>
+            </div>
             <Table
                 bodyData={courses.courses}
                 headerColumns={["#", "Name", "City", "State / Province", "Country", "Date"]}
