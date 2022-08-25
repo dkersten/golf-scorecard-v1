6ab20dc1e65e92ea6986c5f788314143bfcfed50
dkersten <dkersten1994@gmail.com>
2022-08-25 11:40:05 -0400

    added year selection for goals page

--- src/Pages/Goals.js
+++ src/Pages/Goals.js
@@ -1,14 +1,40 @@
 // components
 import Layout from "../Layout"
+import SelectWithState from "../Components/SelectWithState"
 
 // data
 import goals from "../Temp Data/goalsData"
 
+// libraries/helpers
+import { useState } from "react"
+
 const Goals = () => {
+
+    let currentYear = new Date().getFullYear()
+    currentYear = currentYear.toString()
+    const [yearChoice, setYearChoice] = useState(currentYear)
+
+    const SelectOptions = (goals) => {
+        const optionsObj = {}
+        for (const [key, val] of Object.entries(goals)) {
+            optionsObj[key] = val.displayName
+        }
+        return optionsObj
+    }
 
     return(
         <Layout>
-            This is the goals component
+            <div className="year-selector-section">
+                <SelectWithState
+                    selectValue="Year: "
+                    selectId={"yearSelect"}
+                    defaultVal={yearChoice}
+                    currentState={yearChoice}
+                    setCurrentState={setYearChoice}
+                    optionsItems={SelectOptions(goals)}
+                />
+            </div>
+            
         </Layout>
     )
 }
