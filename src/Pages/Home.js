8dde1a757cf55eea8ba4950e6c80875258fc77f1
dkersten <dkersten1994@gmail.com>
2022-11-22 10:57:15 -0500

    started work on the home page component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,23 @@
 // components
 import Layout from "../Layout"
+import BtnPrimary from "../Components/BtnPrimary"
+
+// styling
+import '../styling/Home.scss'
 
 const Home = () => {
     return(
         <Layout>
-            This is the home page
+            <h1>
+                Add a new...
+            </h1>
+            <div className="forms-button-container">
+                <BtnPrimary text="Round" />
+                <BtnPrimary text="Course Played" />
+                <BtnPrimary text="Set Yardages" />
+                <BtnPrimary text="Seasonal Goal" />
+                <BtnPrimary text="Milestone" />
+            </div>
         </Layout>
     )
 }
