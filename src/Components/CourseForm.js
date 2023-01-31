0a2fdc8cda4928b446abd04e9c1b18620871ef33
dkersten <dkersten1994@gmail.com>
2023-01-30 22:00:33 -0500

    added base styling to course form

--- src/Components/CourseForm.js
+++ src/Components/CourseForm.js
@@ -1,6 +1,9 @@
 // libraries/helpers
 import { useState } from "react"
 
+// styling
+import '../styling/components/CoursesForm.scss'
+
 const CourseForm = () => {
 
     const [name, setName] = useState("")
@@ -19,63 +22,80 @@ const CourseForm = () => {
 
     return(
         <form
+            className="form modal-form courses-form"
             onSubmit={(e) => {
                 e.preventDefault()
                 postFormData()
             }}
         >
+            <div className="input-container flex-container">
+                <label
+                    htmlFor="name"
+                    className="flex-item flex-container"
+                >
+                    <span>Name:</span>
+                    <input
+                        id="name"
+                        value={name}
+                        placeholder="Brighton Park GC"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </label>
 
-            <label htmlFor="name">
-            Name: 
-            <input
-                id="name"
-                value={name}
-                placeholder="Name"
-                onChange={(e) => setName(e.target.value)}
-            />
-            </label>
+                <label
+                    htmlFor="city"
+                    className="flex-item flex-container"
+                >
+                    <span>City:</span>
+                    <input
+                        id="city"
+                        value={city}
+                        placeholder="Tonawanda"
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                </label>
 
-            <label htmlFor="city">
-            City: 
-            <input
-                id="city"
-                value={city}
-                placeholder="City"
-                onChange={(e) => setCity(e.target.value)}
-            />
-            </label>
+                <label
+                    htmlFor="state"
+                    className="flex-item flex-container"
+                >
+                    <span>State:</span>
+                    <input
+                        id="state"
+                        value={state}
+                        placeholder="NY"
+                        onChange={(e) => setState(e.target.value)}
+                    />
+                </label>
 
-            <label htmlFor="state">
-            State: 
-            <input
-                id="state"
-                value={state}
-                placeholder="State"
-                onChange={(e) => setState(e.target.value)}
-            />
-            </label>
+                <label
+                    htmlFor="country"
+                    className="flex-item flex-container"
+                >
+                    <span>Country:</span> 
+                    <input
+                        id="country"
+                        value={country}
+                        placeholder="USA"
+                        onChange={(e) => setCountry(e.target.value)}
+                    />
+                </label>
 
-            <label htmlFor="country">
-            Country: 
-            <input
-                id="country"
-                value={country}
-                placeholder="Country"
-                onChange={(e) => setCountry(e.target.value)}
-            />
-            </label>
+                <label
+                    htmlFor="year"
+                    className="flex-item flex-container"
+                >
+                    <span>Year:</span> 
+                    <input
+                        id="year"
+                        value={year}
+                        placeholder={ new Date().getFullYear() }
+                        onChange={(e) => setYear(e.target.value)}
+                    />
+                </label>
+            </div>
 
-            <label htmlFor="year">
-            Year: 
-            <input
-                id="year"
-                value={year}
-                placeholder="Year"
-                onChange={(e) => setYear(e.target.value)}
-            />
-            </label>
-
-            <input type="submit" value="Submit" />
+            <input className="btn-primary" type="submit" value="Submit" />
         </form>
     )
 }
